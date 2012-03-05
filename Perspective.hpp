2d51e1b587a5e158e611564fb3eeac116f8f8c2b
Mika Turunen <mika.turunen@ymail.com>
2012-03-05 11:49:54 +0200

    Additional comments for function

--- Perspective.hpp
+++ Perspective.hpp
@@ -4,6 +4,17 @@
 // NOTE : depends on opengl headers and libraries, naturally. :)
 
 namespace Utilities {
+	/*!
+		Sets perspective. Used to replace gluPerspective.
+		@param fov
+			Field of view.
+		@param aspectRatio
+			Aspect ratio of the screen; generally speaking width of screen divided by height.
+		@param zNear
+			Z-near clipping plane.
+		@param zFar
+			Z-far clipping plane.
+	*/
 	void SetPerspective(const float &fov, const float &aspectRatio, const float &zNear, 
 		const float &zFar) {
 		const float PI = 3.14159265f;
